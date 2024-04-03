2ed1c51c8950588fcd889c578265ac560a80d1ca
scamgi <scampini.giacomo@gmail.com>
2024-04-03 13:39:22 +0200

    I wrote the code to diplay the books object on the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [books, setBooks] = useState([
+    {
+      id: 0,
+      book_title: "lorem",
+      book_description: "lorem",
+      book_price: 12.99,
+      book_cover_link: "link",
+    },
+  ]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/books", { method: "GET" })
+      .then((chunk) => chunk.json())
+      .then((data) => setBooks(data));
+  }, []);
 
   return (
     <main>
       <div className="container">
-        <div className="book">
-          <div className="book__cover">
-            <img src="qualcosa.jpg" alt="book cover" />
+        {books.map((book) => (
+          <div key={book.id} className="book">
+            <div className="book__cover">
+              <img src={book.book_cover_link} alt="book cover" />
+            </div>
+            <h1 className="book__title">{book.book_title}</h1>
+            <p className="book__description">{book.book_description}</p>
+            <p className="book__price">{book.book_price} &euro;</p>
+            <button>Update</button>
+            <button>Delete</button>
           </div>
-          <h1 className="book__title">Book title</h1>
-          <p className="book__description">Description</p>
-          <p className="book__price">Price</p>
-          <button>Update</button>
-          <button>Delete</button>
-        </div>
-        <div className="book">
-          <div className="book__cover">
-            <img src="qualcosa.jpg" alt="book cover" />
-          </div>
-          <h1 className="book__title">Book title</h1>
-          <p className="book__description">Description</p>
-          <p className="book__price">Price</p>
-          <button>Update</button>
-          <button>Delete</button>
-        </div>
+        ))}
       </div>
     </main>
   );
