67af2c7cab9d355f88511775aa7e105e0234a65d
scamgi <scampini.giacomo@gmail.com>
2024-04-03 13:22:32 +0200

    interface created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>Ciao mondo.</div>;
+  return (
+    <main>
+      <div className="container">
+        <div className="book">
+          <div className="book__cover">
+            <img src="qualcosa.jpg" alt="book cover" />
+          </div>
+          <h1 className="book__title">Book title</h1>
+          <p className="book__description">Description</p>
+          <p className="book__price">Price</p>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+        <div className="book">
+          <div className="book__cover">
+            <img src="qualcosa.jpg" alt="book cover" />
+          </div>
+          <h1 className="book__title">Book title</h1>
+          <p className="book__description">Description</p>
+          <p className="book__price">Price</p>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
